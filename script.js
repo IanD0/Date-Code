9a598229ed6c3311594a9613e9e2810829ca8a0c
Ian <116680072+IanD0@users.noreply.github.com>
2024-05-31 15:27:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,40 +2,38 @@ const btn = document.querySelector(".no");
 let position = 0;
 
 const moveButton = () => {
-position = position ? 0 : 150;
-btn.style.transform = `translate(${position}px, 0px)`;
-btn.style.transition = "all 0.2s ease";
+    position = position ? 0 : 150;
+    btn.style.transform = `translate(${position}px, 0px)`;
+    btn.style.transition = "all 0.2s ease";
 };
 
-btn.addEventListener("click", moveButton);
 btn.addEventListener("mouseover", moveButton);
-
 
 const sim = document.getElementById('yes');
 
 sim.addEventListener("click", () => {
-
-let timerInterval
-Swal.fire({
- title: 'Obrigado 😍',
- html: 'Deixa o resto comigo. 💘',
- timer: 2000,
- timerProgressBar: true,
- didOpen: () => {
-Swal.showLoading()
-const b = Swal.getHtmlContainer().querySelector('b')
-timerInterval = setInterval(() => {
-  b.textContent = Swal.getTimerLeft()
-}, 100)
-},
-willClose: () => {
- clearInterval(timerInterval)
-}
-}).then((result) => {
-/* Read more about handling dismissals below */
- if (result.dismiss === Swal.DismissReason.timer) {
-console.log('I was closed by the timer')
-}
-})
-
+    let timerInterval;
+    Swal.fire({
+        title: 'Obrigado 😍',
+        html: 'Deixa o resto comigo. 💘',
+        timer: 2000,
+        timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading();
+            const b = Swal.getHtmlContainer().querySelector('b');
+            timerInterval = setInterval(() => {
+                b.textContent = Swal.getTimerLeft();
+            }, 100);
+        },
+        willClose: () => {
+            clearInterval(timerInterval);
+            window.open("https://api.whatsapp.com/send?phone=+5571986908525&text=Aceito+sim+vida%21+😍", "_blank");
+        }
+    }).then((result) => {
+        /* Read more about handling dismissals below */
+        if (result.dismiss === Swal.DismissReason.timer) {
+            console.log('I was closed by the timer');
+        }
+        
+    });
 });
